6b55f7deed5ff1bc06a2b82b749dfecfb5cf7933
Daniil Z <idanix@list.ru>
2022-07-01 19:26:47 +0300

    Mavenize plugin

---
+++ src/java/me/matterz/supernaturals/util/Location.java
@@ -0,0 +1,42 @@
+package me.matterz.supernaturals.util;
+
+import me.matterz.supernaturals.SupernaturalsPlugin;
+import org.bukkit.World;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+
+	/**
+	 * Auto-Generated serialVersionUID
+	 */
+	private static final long serialVersionUID = 8884729998863928105L;
+
+	private double x;
+	private double y;
+	private double z;
+	private String world;
+
+	public Location(org.bukkit.Location location) {
+		x = location.getX();
+		y = location.getY();
+		z = location.getZ();
+		world = location.getWorld().getName();
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public World getWorld() {
+		return SupernaturalsPlugin.instance.getServer().getWorld(world);
+	}
+}
